b62d4d9f00959d3e5671c991c7fb95e3cd2fd685
Michael-Galindez <michaelgalindez274@gmail.com>
2020-02-05 18:18:00 +0000

    Hello World (C programming)

---
+++ 0x00-hello_world/.#5-printf.c
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.18476:1580494883
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+B#include<stdio.h>
+int main() {
+char charType;
+int intType;
+long int longType;
+long long int longlongType;
+float floatType;
+printf("Size of int: %ld bytes\n", sizeof(charType));
+printf("Size of float: %ld bytes\n", sizeof(intType));
+printf("Size of double: %ld bytes\n", sizeof(longType));
+printf("Size of char: %ld byte\n", sizeof(longlongType));
+printf("Size of char: %ld byte\n", sizeof(floatType));
+return(0);
+}
