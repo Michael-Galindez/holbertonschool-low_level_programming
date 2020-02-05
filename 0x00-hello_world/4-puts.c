c2a4e834b93ed2c473545696f4326f8441975f16
Michael-Galindez <michaelgalindez274@gmail.com>
2020-02-05 18:26:37 +0000

    Puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+/* This is a sample of the "puts" command to used to print in the screen */
 int main(void){
 puts("\"Programming is like building a multilingual puzzle");
 return(0);
