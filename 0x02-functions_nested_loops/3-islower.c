f625680ba6764f51c02afa4b2cbec67c40ba9981
Michael-Galindez <michaelgalindez274@gmail.com>
2020-02-11 19:37:31 +0000

    Islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,12 @@
 #include "holberton.h"
+/**
+ * _islower - Return 1 if letter is lowercase, 0 otherwise.
+ * @c: var char type letter
+ * Return: 1 if lowercase, 0 if not.
+ */
 int _islower(int c)
 {
-	if (c >= 97 && c <= 222)
+	if (c >= 96 && c <= 123)
 {
 return (1);
 }
