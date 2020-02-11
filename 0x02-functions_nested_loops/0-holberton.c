45fc0f4caf458e74927bd1d4e772f02cd662f3ef
Michael-Galindez <michaelgalindez274@gmail.com>
2020-02-11 19:24:53 +0000

    _Putchar Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,9 @@
 #include "holberton.h"
+/**
+ * main - Print "Holberton" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
 int main(void)
 {
 	_putchar('H');
