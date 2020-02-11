2d61a19a3e068eb51236f0e81d1d2a64e101966b
Michael-Galindez <michaelgalindez274@gmail.com>
2020-02-11 20:07:38 +0000

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,23 +2,24 @@
 /**
  * print_sign - Print the sign of a number
  * @n: type int, negative or positive
- * Description: print +, 0, or - depending on number, along with return
+ * Description: print +, 0, or - 
  * Return: 1 if +, 0 if 0, and -1 if -
  */
 int print_sign(int n)
 {
 	if (n > 0)
-{
-	_putchar('+');
-	return (1);
-}
-	else if (n == 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
 	{
 		_putchar('0');
 		return (0);
-	}
-	else {
-		_putchar('-');
-			return (-1);
 	}
 }
