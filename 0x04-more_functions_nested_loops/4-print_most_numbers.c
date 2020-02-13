9f52403197a9226ef9961cf912a29f91661a404b
Michael-Galindez <michaelgalindez274@gmail.com>
2020-02-13 18:03:43 +0000

    More Functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * main - print numbers to 9 excluding 2 and 4.
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+			if (a != 2 && a != 4)
+				_putchar(a + '0');
+			a++;
+
+	}
+	_putchar('\n');
+}
