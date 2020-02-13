56b5d522bfddae2aed281e3426e1e3ca0fa83a64
Michael-Galindez <michaelgalindez274@gmail.com>
2020-02-13 18:30:43 +0000

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,16 +5,15 @@
  */
 void print_most_numbers(void)
 {
-	int a = 0;
+	int a;
 
+	a = 0;
 	while (a < 10)
 	{
 			if (a != 2 && a != 4)
 				_putchar(a + '0');
+
 			a++;
-	}
-	else {
-		return (1);
 	}
 	_putchar('\n');
 }
