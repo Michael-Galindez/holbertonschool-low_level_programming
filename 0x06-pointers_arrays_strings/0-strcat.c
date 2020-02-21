ab5c8e682b902c7959f7580ea83696e7c74cccc8
root <root@localhost.localdomain>
2020-02-20 16:48:36 -0800

    Concatenate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,21 +3,27 @@
  * _strcat - concatenates 2 strings.
  * @src: string 1
  * @dest: string 2
- * Return: the result on dest string.
+ * Return: the thing on dest string.
  */
 char *_strcat(char *dest, char *src)
 {
-int a = 0;
-int b = 0;
+	char *cat = dest;
+	unsigned int d, o;
 
- while (*(dest + a))
-   {
-   a++;
-   }
- while ((*(dest + a) = *(src + b)))
-   {
-	a++;
-	b++;
-   }	
-	return (dest);
+	d = 0;
+	while (dest[d])
+	{
+		cat[d] = dest[d];
+		d++;
+	}
+	o = 0;
+	while (src[o])
+	{
+		cat[d] = src[o];
+		o++;
+		d++;
+	}
+	cat[d] = '\n';
+	cat[d] = '\0';
+	return (cat);
 }
