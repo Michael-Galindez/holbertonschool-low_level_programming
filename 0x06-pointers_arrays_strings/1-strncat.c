a561cfc53fabf3fe8986e1735da5eacf0b914661
root <root@localhost.localdomain>
2020-02-20 16:58:49 -0800

    concatenate strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,26 @@
 #include "holberton.h"
+/**
+ * _strncat - concatenate two strings with n 
+ * @dest: string to concate
+ * @src: string to concate
+ * @n: number of chars to copy
+ * Return: the pointer to the dest thing
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *p = dest;
+	int d = 0;
 
-strncat(char *dest, const char *src, size_t n)
-           {
-               size_t dest_len = strlen(dest);
-               size_t i;
-
-               for (i = 0 ; i < n && src[i] != '\0' ; i++)
-                   dest[dest_len + i] = src[i];
-               dest[dest_len + i] = '\0';
-
-               return dest;
-           }
+	while (*dest++)
+		;
+	dest--;
+	while (*src && d < n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		d++;
+	}
+	*dest = '\0';
+	return (p);
+}
