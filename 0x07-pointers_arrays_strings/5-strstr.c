cb8a36f99a63929084508ee4084dde31c3ea0139
root <root@localhost.localdomain>
2020-02-24 17:14:44 -0800

    Fix 2.2

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,24 +7,24 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i,
-	  int j;
-	int start;
+unsigned int i = 0, j = 0;
 
-	i = j = 0;
-	while (haystack[i] != '\0')
-	{
-		start = i;
-		j = 0;
-		while (haystack[i] == needle[j] &&
-		       needle[j] != '\0' && haystack[i] != '\0')
-		{
-			i++;
-			j++;
-		}
-		if (needle[j] == '\0')
-			return (haystack + start);
-		i = start + 1;
-	}
-	return (NULL);
+while (haystack[i])
+{
+while (needle[j] && (haystack[i] == needle[0]))
+{
+if (haystack[i + j] == needle[j])
+j++;
+else
+break;
+}
+if (needle[j])
+{
+i++;
+j = 0;
+}
+else
+return (haystack + i);
+}
+return (0);
 }
