32cbab69bc3685297182afe17b4ec7cd22a5d90b
root <root@DESKTOP-N8FL129.localdomain>
2020-02-26 14:22:06 -0500

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,39 +1,41 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
+ * checksqtr - check the code for Holberton School students.
+ * @a: integer a
+ * @b: integer b
  * Return: Always 0.
  */
 int checksqtr(int a, int b)
 {
-  if (a == (b * b))
-    {
-      return (b);
-    }
-  if (b * b > a)
-    {
-      return (-1);
-      }
-  return(checksqtr(a, b + 1));
+if (a == (b * b))
+{
+return (b);
 }
+if (b * b > a)
+{
+return (-1);
+}
+return (checksqtr(a, b + 1));
+}
+
 /**
- * main - check the code for Holberton School students.
- *
+ * _sqet_recursion - check the code for Holberton School students.
+ * @n: int number
  * Return: Always 0.
  */
 int _sqrt_recursion(int n)
 {
-  if (n == 1)
-    {
-      return (1);
-      }
-  if (n == 0)
-    {
-      return (0):
-    }
-  if (n < 1)
-    {
-    return (-1);
-    }
-  return (checksqtr(n, 1));
+if (n == 1)
+{
+return (1);
+}
+if (n == 0)
+{
+return (0);
+}
+if (n < 1)
+{
+return (-1);
+}
+return (checksqtr(n, 1));
 }
