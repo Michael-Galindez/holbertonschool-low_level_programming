4c9df6c234ec9e0d3e4a5a36f5adb013207acaff
root <root@DESKTOP-N8FL129.localdomain>
2020-02-26 14:14:28 -0500

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,7 +4,36 @@
  *
  * Return: Always 0.
  */
+int checksqtr(int a, int b)
+{
+  if (a == (b * b))
+    {
+      return (b);
+    }
+  if (b * b > a)
+    {
+      return (-1);
+      }
+  return(checksqtr(a, b + 1));
+}
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int _sqrt_recursion(int n)
 {
-
+  if (n == 1)
+    {
+      return (1);
+      }
+  if (n == 0)
+    {
+      return (0):
+    }
+  if (n < 0)
+    {
+    return (-1);
+    }
+  return (checksqtr(n, 1));
 }
