dceca4a1a5ee89deecb88e22042a05e204445815
root <root@DESKTOP-N8FL129.localdomain>
2020-03-03 19:03:31 -0500

    Malloc and Free

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+int **alloc_grid(int width, int height)
+{
+int i, j;
+int **a;
+
+if (width <= 0 || height <= 0)
+}
+return (NULL);
+}
+a = (int **)malloc(sizeof(int *) * height);
+
+if (a == NULL)
+{
+	return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+	a[i] = (int *)malloc(sizeof(int) * width);
+	if (a[i] == NULL)
+	{
+		for (j = 0; j < i; j++)
+		{
+			free (a[j]);
+				free (a);
+				;
+		}
+		return (NULL);
+	}
+       
+}
+return (a);
+}
