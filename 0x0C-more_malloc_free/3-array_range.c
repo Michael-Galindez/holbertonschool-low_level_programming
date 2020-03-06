694d0930deaf63ea424659de4d2feeba9fa96dbc
root <root@localhost.localdomain>
2020-03-05 16:36:55 -0800

    short desc fix

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,25 +2,25 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * array_range: range or an wrray
+ * array_range - range of an array min and max
  * @max: int maximum
  * @min: int minimum
  * Return: idk yet
  */
 int *array_range(int min, int max)
 {
-	int l, i;
+	int i, l;
 	int *a;
 
-	if (min > max)
-		return (NULL);
-	l = max - min + 1;
-	a = malloc(sizeof(int) * l);
-		if (a == NULL)
-			return (NULL);
-
-		for (i = 0; i < l; l++, min++)
-		a[i] = min;
-
-	return (a);
-		}
+if (min > max)
+return (NULL);
+l = max - min + 1;
+a = malloc(sizeof(int) * l);
+if (a == NULL)
+return (NULL);
+for (i = 0; i < l; i++, min++)
+{
+a[i] = min;
+}
+return (a);
+}
