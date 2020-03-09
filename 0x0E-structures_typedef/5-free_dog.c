32acb5f31fd12975cb2b63bcabbda8091aac625e
root <root@DESKTOP-N8FL129.localdomain>
2020-03-09 16:05:53 -0500

    Free the DOGS

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees the DOGSSS
+ * @d: pointer to dog
+ * Return: voids
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
