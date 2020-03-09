0e0c4afe960996d4024ee772d200de73e9a7330b
root <root@DESKTOP-N8FL129.localdomain>
2020-03-09 15:55:15 -0500

    fix print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		d->name = "(nil)";
-			if (d->owner == NULL)
-				d->owner = "(nil)";
-	   printf("Name: %s\nAge: %lf\nOwner: %s\n", d->name, d->age, d->owner);
+     	if (d->owner == NULL)
+       	       	d->owner = "(nil)";
+	   printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
