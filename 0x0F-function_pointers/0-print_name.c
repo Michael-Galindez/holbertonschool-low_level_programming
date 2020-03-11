61922bf7ad028844074f7b500869efa84fcf3faf
Michael-Galindez <60362631+Michael-Galindez@users.noreply.github.com>
2020-03-11 11:08:52 -0500

    Betty fixes

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,5 +2,5 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
-		f(name);
+		(name);
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,6 +2,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
+
 	if (array && action)
 		while (i < size)
 			action(arrai[i]);
