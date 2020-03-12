24dd6a27294468278de4c98864d8896ddb393b9f
root <root@localhost.localdomain>
2020-03-11 19:27:34 -0700

    debian fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
 #include "function_pointers.h"
 /**
 * print_name - calls a f to print a name
- *@name:the name
- *@f:the function
- */
+* @name:the name
+* @f:the function
+*/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
