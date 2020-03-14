beabbed749c782917d80fb19ad94dc204acbfc31
root <root@DESKTOP-N8FL129.localdomain>
2020-03-14 12:20:36 -0500

    fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,17 +1,20 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 void print_c (va_list c)
 {
-	printf("%c", va_arg(char, c));
+	printf("%c", va_arg(c, int));
 }
 
-void print_s (va_list i)
+void print_string (va_list string)
 {
 	char *str;
 
-	str = va_arg(s, char *);
+	str = va_arg(string, char *);
 		if (str == NULL)
-			str = (nil);
+			str = "(nil)";
 	printf("%s", str);
-};
+}
 
 void print_i (va_list i)
 {
@@ -20,10 +23,40 @@ void print_i (va_list i)
 
 void print_f (va_list f)
 {
-	printf("%f", va_arg(f, float));
+	printf("%f", va_arg(f, double));
 }
 void print_all(const char * const format, ...)
 {
 
+	unsigned int i, j;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_string},
+		{"i", print_i},
+		{"f", print_ f},
+		{NULL, NULL}
+	};
+	va_list valist;
+	char *separator = "";
 
+	va_start(valist, format);
+	i = 0;
+	while (format && format[i])
+	{
+		j = 0;
+	while (p[j].t)
+	{
+		if (p[j].t == format[i])
+		{
+			printf("%s", separator);
+			p[j].f(valist);
+			separator = ", ";
+			break;
+		}
+		j++;
+	}
+	i++;
+}
+va_end(valist);
+printf("\n");
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdarg.h>
+typedef struct print
+{
+	char *t;
+	void (*f)(va_list);
+} print_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
