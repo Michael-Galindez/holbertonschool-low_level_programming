3554728f91bc0127c2929cc8a5dd298bc8034ed0
root <root@DESKTOP-N8FL129.localdomain>
2020-03-14 12:50:13 -0500

    HEADER_H_

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,13 @@
+#ifndef VARIADIC_FUNCTIONS_H_
+#define VARIADIC_FUNCTIONS_H_
 #include <stdio.h>
 #include <stdarg.h>
+
+/**
+ * struct print - structure for 3 point
+ * @t: pointer char
+ * @f: function pointer
+ */
 typedef struct print
 {
 	char *t;
@@ -9,3 +17,4 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+#endif
