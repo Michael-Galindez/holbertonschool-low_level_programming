57102782bf04720bad03f37b502d631f5d0b3652
Michael-Galindez <michaelgalindez274@gmail.com>
2020-03-19 16:32:25 -0500

    fixing logic and adding add-node func, to add a head node to alist

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * add_node - adds a node to the linked list
+ * @head: pointer to a linked list
+ * @str: string to add to the new node
+ * Return: pointer to the new value or node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *new;
+
+if (str == NULL)
+return (NULL);
+
+new = malloc(sizeof(list_t));
+if (new == NULL)
+return (NULL);
+
+new->str = strdup(str);
+if (new->str == NULL)
+{
+free(new);
+return (NULL);
+}
+new->len = _strlen(new->str);
+new->next = *head;
+*head = new;
+
+return (new);
+}
