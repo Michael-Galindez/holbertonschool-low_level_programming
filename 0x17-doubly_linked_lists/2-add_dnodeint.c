1344ba18dc7a85324f80bc601551bacfcbf8b420
R6Doc <michaelgalindez274@gmail.com>
2020-05-03 12:28:43 -0500

    adding files again

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add a note to the list
+ * @h: pointer to the start of the list
+ * Return: return node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+
+}
