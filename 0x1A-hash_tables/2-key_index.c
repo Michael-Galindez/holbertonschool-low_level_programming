d5ea8c71b26ea695b8da47cf409e6207d8fa2e4c
R6Doc <michaelgalindez274@gmail.com>
2020-05-29 13:51:09 -0500

    fixing indent

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,11 +1,14 @@
 #include "hash_tables.h"
 
 /**
- * key_index -
- *
- * Return: 
+ * key_index - Gives key index
+ * @key: Key
+ * @size: table size
+ * Return: index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+
+return (hash_djb2(key) % size);
 
 }
